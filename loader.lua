9de3733d4a33556997b5bda428364ea056d4c09c
Neczx <lnwzaaaaaaa55@gmail.com>
2026-06-03 14:53:37 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,38 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local executor =
+    identifyexecutor and identifyexecutor()
+    or getexecutorname and getexecutorname()
+    or "Unknown"
+
+local supported = {
+    ["synapse z"] = true,
+    ["wave"] = true,
+    ["volt"] = true,
+    ["seliware"] = true,
+    ["potassium"] = true,
+    ["solara"] = true,
+    ["xeno"] = true,
+    ["delta"] = true,
+    ["arceus x"] = true,
+    ["codex"] = true,
+}
+
+local found = false
+
+for name in pairs(supported) do
+    if string.find(string.lower(executor), name) then
+        found = true
+        break
+    end
+end
+
+if not found then
+    game.Players.LocalPlayer:Kick("Executor ไม่รองรับ")
+    return
+end
+
 local Success, Result = pcall(function()
     return game:HttpGet("https://api-survive-zombie-arena.vercel.app/api/loader")
 end)
